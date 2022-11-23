7d38391d51aba4fdf2e75c0c37e429177497af91
KaroVillarraga <karovilla@gmail.com>
2022-11-23 10:47:36 +0100

    listado de productos

--- products.js
+++ products.js
@@ -2,9 +2,57 @@ const product = [
     {
         name : "Modern Lamp",
         image: "http....",
-        id: 1,
-        price : 20.00,
+        id: 01,
+        price : 50.00,
     },
-    {},
-    {},
+    {
+        name : "Coffee Chair",
+        image: "http....",
+        id: 02,
+        price : 20.00,
+    },   
+    {
+        name : "Simple Desk",
+        image: "http....",
+        id: 03,
+        price : 60.00,
+    },    {
+        name : "Modern Lamp",
+        image: "http....",
+        id: 04,
+        price : 20.00,
+    },    {
+        name : "Desk",
+        image: "http....",
+        id: 05,
+        price : 80.00,
+    },    {
+        name : "Lamp",
+        image: "http....",
+        id: 06,
+        price : 20.00,
+    },    {
+        name : "Chair",
+        image: "http....",
+        id: 07,
+        price : 60.00,
+    },
+    {
+        name : "Arm Chair",
+        image: "http....",
+        id: 08,
+        price : 40.00,
+    },
+    {
+        name : "Chair",
+        image: "http....",
+        id: 09,
+        price : 45.00,
+    },
+    {
+        name : "Chair",
+        image: "http....",
+        id: 10,
+        price : 30.00,
+    },
 ]

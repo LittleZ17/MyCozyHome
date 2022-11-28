bee78c10e4ad5246ef187d86c563cc33de2b5012
KaroVillarraga <karovilla@gmail.com>
2022-11-28 13:05:15 +0100

    grid template area mobile

--- js/index.js
+++ js/index.js
@@ -3,19 +3,22 @@ const productsCard = document.querySelector("#cards");
 function printProducts(productsArray) {
   productsArray.forEach((item) => {
     productsCard.innerHTML += `
-    <div class="card">
-    <img src= "${item.image}" class="card-img-top" alt="${item.name}" />
-    <div class="card-body">
-      <h5 class="card-title">${item.name}</h5>
-      <p class="card-text">${item.price} €</p>
+    <div class="products">
+      <div class="item">
+        <div class="imgProduct">
+          <img src= "${item.image}" class="card-img-top" alt="${item.name}" />
+        </div>
+      <div class="infoProduct">
+        <h5 class="card-title">${item.name}</h5>
+        <a href="#"><img src="assets/icons/buttonCart.svg" alt="" /></a>
+        <p class="card-text">${item.price} €</p>
       <div>
         <button><img src="assets/icons/buttonMinus.svg" alt="" /></button>
-        <input type="number" />
+        <h6>01</h6>
         <button><img src="assets/icons/buttonAdd.svg" alt="" /></button>
       </div>
-      <a href="#"><img src="assets/icons/buttonCart.svg" alt="" /></a>
     </div>
-    <a href="#"><img src="assets/icons/buttonCart.svg" alt="" /></a>
+  </div>
   </div>
   `;
   });
